361cbdb0640941ed8d179214dd62b9c6d623de93
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2022-05-26 15:36:51 +0400

    открытие видео по клику на затемнённой области

--- source/js/modules/play-video.js
+++ source/js/modules/play-video.js
@@ -2,12 +2,18 @@ const videoContainer = document.querySelector('.gym__video-container');
 const buttonPlay = videoContainer.querySelector('button');
 const videoBlock = document.querySelector('.gym__video-container iframe');
 
+const clickOnVideo = () => {
+  videoBlock.style.zIndex = 10002;
+  videoBlock.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+};
+
 const playVideo = () => {
   buttonPlay.addEventListener('click', () => {
-    videoBlock.style.zIndex = 10002;
-    console.log(videoBlock.src += "?autoplay=1");
-    videoBlock.src += "&autoplay=1"
+    clickOnVideo();
+  });
 
+  videoContainer.addEventListener('click', () => {
+    clickOnVideo();
   });
 };
 
