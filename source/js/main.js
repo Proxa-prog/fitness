c9b863444ba256d181fb4948f3adb7ac9ae92d46
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2022-06-07 16:08:48 +0400

    слайдер

--- source/js/main.js
+++ source/js/main.js
@@ -4,11 +4,19 @@ import playVideo from './modules/play-video';
 import changeSelection from './modules/subscription-carousel';
 import slider from './modules/treners-carousel';
 import {buttonClickPrevious, buttonClickForvard} from './modules/button-click';
-const buttonBack = document.querySelector('.treners__button-back');
-const buttonForward = document.querySelector('.treners__button-forward');
-const slides = document.querySelectorAll('.treners__item');
-let slideIndex = 0;
-
+const buttonBackTreners = document.querySelector('.treners__button-back');
+const buttonForwardTreners = document.querySelector('.treners__button-forward');
+const buttonBackComment = document.querySelector('.reviews__button-prev');
+const buttonForwardComment = document.querySelector('.reviews__button-next');
+const slidesTreners = document.querySelectorAll('.treners__item');
+const slidesListTreners = document.querySelector('.treners__list');
+const slidesListComment = document.querySelector('.reviews__user-comment-wrapper');
+const comments = document.querySelectorAll('.reviews__comment');
+let slidesArray = Array.from(slidesTreners);
+let newArrayTreners = slidesArray.slice();
+let commentArray = Array.from(comments);
+let newArrayComment = commentArray.slice();
+let currentWidth = 0;
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -21,11 +29,54 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
   // ---------------------------------
 
+  // Определение количества карточек при загрузке разрешения экрана
+
+  const checkWindowWidth = () => {
+    let windowWidth = document.documentElement.clientWidth;
+
+    if (windowWidth >= 1279) {
+      currentWidth = 4;
+    }
+
+    if (windowWidth >= 768 && windowWidth < 1279) {
+      currentWidth = 2;
+    }
+
+    if (windowWidth < 768) {
+      currentWidth = 1;
+    }
+  };
+
+  // Определение количества карточек при изменениее разрешения
+
+  window.addEventListener('resize', () => {
+    let windowWidth = document.documentElement.clientWidth;
+
+    if (windowWidth > 1024) {
+      currentWidth = 4;
+      slider(newArrayTreners, '.treners__item', '.treners__list', currentWidth);
+    }
+
+    if (windowWidth < 1279 && windowWidth > 767) {
+      currentWidth = 2
+      slider(newArrayTreners, '.treners__item', '.treners__list', currentWidth);
+    }
+
+    if (windowWidth < 768) {
+      currentWidth = 1;
+      slider(newArrayTreners, '.treners__item', '.treners__list', currentWidth);
+    }
+  });
+
+  checkWindowWidth();
   playVideo();
   changeSelection('subscription__item-month-current', 'subscription__item-month');
-  slider(slideIndex, false);
-  buttonClickPrevious(buttonBack, slideIndex, slides, slider);
-  buttonClickForvard(buttonForward, slideIndex, slides, slider);
+  slider(newArrayTreners, '.treners__item', '.treners__list', currentWidth);
+  buttonClickPrevious(buttonBackTreners, slider, newArrayTreners, '.treners__item', '.treners__list', currentWidth);
+  buttonClickForvard(buttonForwardTreners, slider, newArrayTreners, '.treners__item', '.treners__list', currentWidth);
+  slider(newArrayComment, '.reviews__comment', '.reviews__user-comment-wrapper', 1);
+  buttonClickPrevious(buttonBackComment, slider, newArrayComment, '.reviews__comment', '.reviews__user-comment-wrapper', 1);
+  buttonClickForvard(buttonForwardComment, slider, newArrayComment, '.reviews__comment', '.reviews__user-comment-wrapper', 1);
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
---
+++ source/js/modules/constants.js
@@ -0,0 +1,11 @@
+const treners = {
+  buttonBack: document.querySelector('.treners__button-back'),
+  buttonForward: document.querySelector('.treners__button-forward'),
+  itemClass: '.treners__item',
+  listClass: '.treners__list',
+  countDesktop: 4,
+  countTablet: 2,
+  countMobile: 1,
+}
+
+export default treners;
