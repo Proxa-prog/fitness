1630df1c7b79b4abce0577a9489e1829947f4d83
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2022-06-10 00:11:40 +0400

    правки 10.06.

--- source/js/modules/treners-carousel.js
+++ source/js/modules/treners-carousel.js
@@ -1,6 +1,10 @@
 const slider = (newArray, allItem, list, count) => {
   const slidesTreners = document.querySelectorAll(allItem);
   const slidesList = document.querySelector(list);
+  const buttonForwardTreners = document.querySelector('.treners__button-forward');
+  const buttonBackTreners = document.querySelector('.treners__button-back');
+  let dataSetCount = 0;
+
 
   for (let j = 0; j < slidesTreners.length; j++) {
     slidesTreners[j].remove();
@@ -9,6 +13,23 @@ const slider = (newArray, allItem, list, count) => {
   for (let i = 0; i < newArray.length; i++) {
     if (i < count) {
       slidesList.appendChild(newArray[i]);
+
+      if (newArray[i].dataset.last) {
+        buttonForwardTreners.disabled = true;
+      } else {
+        buttonForwardTreners.disabled = false;
+      }
+
+
+      if (!newArray[i].dataset.first) {
+        dataSetCount++;
+      }
+
+      if (dataSetCount > 3) {
+        buttonBackTreners.disabled = false;
+      } else {
+        buttonBackTreners.disabled = true;
+      }
     }
   }
 };
